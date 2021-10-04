2472d03d4f344793b1cf0f4c88bcdbe97c0ef99a
Eric Serra <63271342+ericserraupwork@users.noreply.github.com>
2021-10-04 11:44:34 -0400

    Update stop_application.sh

--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -2,5 +2,4 @@
 cd /home/ubuntu/www/project/
 # source /home/ubuntu/www/project-venv/bin/activate
 # supervisorctl -c /home/ubuntu/www/project/supervisor/default.conf stop all 2&>1 >/dev/null
-# sudo unlink /tmp/supervisor.sock 2> /dev/null
 sudo pkill supervisor*
